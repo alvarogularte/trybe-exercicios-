e06dd275198ffd5599d7c55abcc406a871f29cc2
alvarogularte <alvaro.gularte@gmail.com>
2021-09-08 18:22:46 -0300

    Completa o requisito 3 do exercicio

--- fundamentos/bloco-8-introducao-a-higher-order-functions/exercício.js
+++ fundamentos/bloco-8-introducao-a-higher-order-functions/exercício.js
@@ -63,6 +63,8 @@ const books = [
   },
 ];
 
+// Requisito 1
+
 // Adicione o código do exercício aqui:
 // const expectedResult = [
 //   'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
@@ -80,40 +82,82 @@ const books = [
 // console.log(formatedBookNames(books));
 // assert.deepStrictEqual(formatedBookNames(), expectedResult);
 
+// Requisito 2
+
+// const expectedResult = [
+//   {
+//     age: 31,
+//     author: 'Isaac Asimov',
+//   },
+//   {
+//     age: 38,
+//     author: 'H. P. Lovecraft',
+//   },
+//   {
+//     age: 39,
+//     author: 'Stephen King',
+//   },
+//   {
+//     age: 43,
+//     author: 'George R. R. Martin',
+//   },
+//   {
+//     age: 45,
+//     author: 'Frank Herbert',
+//   },
+//   {
+//     age: 62,
+//     author: 'J. R. R. Tolkien',
+//   },
+// ];
+
+// function nameAndAge() {
+//   // escreva seu código aqui
+//   return books.map((book) => ({
+//     author: book.author.name,
+//     age: book.releaseYear - book.author.birthYear
+//   })).sort((object1, object2) => object1.age - object2.age);
+  
+// }
+// console.log(nameAndAge());
+// assert.deepStrictEqual(nameAndAge(), expectedResult);
+
+// Requisito 3
+
 const expectedResult = [
-  {
-    age: 31,
-    author: 'Isaac Asimov',
+  { 
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: { name: 'George R. R. Martin', birthYear: 1948 },
+    releaseYear: 1991
   },
   {
-    age: 38,
-    author: 'H. P. Lovecraft',
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954
   },
   {
-    age: 39,
-    author: 'Stephen King',
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951
   },
   {
-    age: 43,
-    author: 'George R. R. Martin',
-  },
-  {
-    age: 45,
-    author: 'Frank Herbert',
-  },
-  {
-    age: 62,
-    author: 'J. R. R. Tolkien',
-  },
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: { name: 'Frank Herbert', birthYear: 1920 },
+    releaseYear: 1965
+  }
 ];
 
-function nameAndAge() {
+function fantasyOrScienceFiction() {
   // escreva seu código aqui
-  return books.map((book) => ({
-    author: book.author.name,
-    age: book.releaseYear - book.author.birthYear
-  })).sort((object1, object2) => object1.age - object2.age);
-  
+  return books.filter((book) => book.genre === 'Ficção Científica' || book.genre === 'Fantasia');
 }
-console.log(nameAndAge());
-assert.deepStrictEqual(nameAndAge(), expectedResult);
+console.log(fantasyOrScienceFiction());
+assert.deepStrictEqual(fantasyOrScienceFiction(), expectedResult);
