668c1f7f9e2d447b59de0c3f44b4c692f023347b
alvarogularte <alvaro.gularte@gmail.com>
2021-09-12 15:01:35 -0300

    Completa o requisito 2 do exercicio do dia 8-1

--- fundamentos/bloco-8-introducao-a-higher-order-functions/exercicio-8-1.js
+++ fundamentos/bloco-8-introducao-a-higher-order-functions/exercicio-8-1.js
@@ -1,14 +1,25 @@
-const employees = (name) => {
-  const email = name.toLowerCase().split(' ').join('_');
-  return { name, email: `${email}@trybe.com`};
-}
-const newEmployees = (callback) => {
-  const employees = {
-    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
-  }
-  return employees;
+// // Requisito 1
+// const employees = (name) => {
+//   const email = name.toLowerCase().split(' ').join('_');
+//   return { name, email: `${email}@trybe.com`};
+// }
+// const newEmployees = (callback) => {
+//   const employees = {
+//     id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+//     id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+//     id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+//   }
+//   return employees;
+// };
+
+// console.log(newEmployees(employees));
+
+// Requisito 2
+const checker = (mynumber, number) => mynumber === number;
+
+const raffle = (myNumber, callback) => {
+  const number = Math.floor((Math.random() * 5) +1);
+  return callback(myNumber, number) ? 'Lucky day, you won!' : 'Bad lucky, try again!'
 };
 
-console.log(newEmployees(employees));
+console.log(raffle(3, checker));
