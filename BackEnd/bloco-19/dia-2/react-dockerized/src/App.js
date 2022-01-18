bcff61f11c4830b7d19caad67b93f437138869f1
alvarogularte <alvaro.gularte@gmail.com>
2022-01-18 18:37:17 -0300

    Exercício de fixação do dia 19.2

---
+++ BackEnd/bloco-19/dia-2/react-dockerized/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Docker World</h1>
+      <p>Cheguei BackEnd!!!</p>
+    </div>
+  );
+}
+
+export default App;
