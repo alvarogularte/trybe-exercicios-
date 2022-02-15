64a701066cfa0126202630cbeea4265d3e0b9939
alvarogularte <alvaro.gularte@gmail.com>
2022-02-15 15:34:02 -0300

    Adiciona exercicios do dia 1

--- BackEnd/bloco-22-Node-js/package-nodejs/exercicios-22.1/index.js
+++ BackEnd/bloco-22-Node-js/package-nodejs/exercicios-22.1/index.js
@@ -1,15 +1,32 @@
-const PESO_80KG = 114;
-const ALTURA_1M_78CM = 175;
+const readline = require('readline-sync');
 
-function calculaImc () {
-  const peso = PESO_80KG;
-  const altura = ALTURA_1M_78CM;
+// Criamos uma lista dos scripts disponíveis
+// Utilizamos objetos com `name` e `script` para facilitar a criação da lista que será exibida
+const scripts = [
+  { name: 'Calcular IMC', script: './imc.js' },
+  { name: 'Calcular velocidade média', script: './velocidade.js' },
+  { name: 'Jogo de adivinhação', script: './sorteio.js' },
+  { name: 'Calcular fatorial', script: './fatorial.js' },
+  { name: 'Exibir n números de fibonacci', script: './fibonacci.js' },
+];
 
-  console.log(`Peso: ${peso}, Altura: ${altura}`);
+// Iteramos sobre os scripts para criar a lista numerada
+let mensagem = scripts
+  .map((script, index) => `${index + 1} - ${script.name}`);
 
-  const imc = (peso / Math.pow(altura / 100, 2)).toFixed(2);
+// Adicionamos uma linha a mais no começo da mensagem
+mensagem.unshift('Escolha um número para executar o script correspondente');
 
-  console.log(`IMC: ${imc}`);
-}
+// Juntamos todos os elementos em uma string, separando-os por uma quebra de linha
+mensagem = mensagem.join('\n');
 
-calculaImc();
+const scriptNumber = readline.questionInt(mensagem) - 1;
+
+const script = scripts[scriptNumber];
+
+if (!script) console.log('Número inválido. Saindo');
+
+// Chamamos o script selecionado
+// Note que, no dia a dia, é mais comum utilizarmos outras formas de executar arquivos externos
+// No entanto, para fins didáticos, o `require` nos atende por enquanto.
+require(script.script);
