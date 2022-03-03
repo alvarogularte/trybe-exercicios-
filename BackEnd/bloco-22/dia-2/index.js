69f2ceac33a19b06835e53c10f54d1cdebc909b2
alvarogularte <alvaro.gularte@gmail.com>
2022-03-03 16:07:18 -0300

    exercicios dia 22.2

---
+++ BackEnd/bloco-22/dia-2/index.js
@@ -0,0 +1,33 @@
+function doMath(a, b, c) {
+  return new Promise((resolve, reject) => {});
+
+  if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number')
+  reject('Informe apenas números');
+
+  const result = (a + b) * c;
+
+      if (result < 50) {
+        return reject('Valor muito baixo');
+      }
+
+      resolve(result);
+}
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100 + 1);
+}
+
+async function callDoMath() {
+  /* Criamos um novo array de 3 posições
+   * e utilizamos o `map` para gerar um número aleatório
+   * para cada posição do Array
+   */
+  const randomNumbers = Array.from({ length: 3 }).map(getRandomNumber);
+
+  try {
+    const result = await doMath(...randomNumbers);
+  console.log(result);
+  } catch (err) {
+    console.error(err);
+  }
+}
