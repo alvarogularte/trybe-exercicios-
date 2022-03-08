c02d49f2c5028537e7741a47f5b858cb5c4798b5
alvarogularte <alvaro.gularte@gmail.com>
2022-03-08 16:04:30 -0300

    [feat] managed transactions

--- BackEnd/bloco-24/dia-2/conteudo/index.js
+++ BackEnd/bloco-24/dia-2/conteudo/index.js
@@ -48,49 +48,75 @@ app.get('/employees/:id', async (req, res) => {
     res.status(500).json({ message: 'Algo deu errado' });
   };
 });
+// app.post('/employees', async (req, res) => {
+//   try {
+//     const { firstName, lastName, age, city, street, number } = req.body;
+
+//     const employee = await Employee.create({ firstName, lastName, age });
+
+//     await Address.create({ city, street, number, employeeId: employee.id });
+
+//     return res.status(201).json({ message: 'Cadastrado com sucesso' });
+//   } catch (e) {
+//     console.log(e.message);
+//     res.status(500).json({ message: 'Algo deu errado' });
+//   }
+// });
+
+// app.post('/employees', async (req, res) => {
+//   // Primeiro iniciamos a transação
+//   const t = await sequelize.transaction();
+
+//   try {
+//     const { firstName, lastName, age, city, street, number } = req.body;
+
+//     // Depois executamos as operações
+//     const employee = await Employee.create(
+//       { firstName, lastName, age },
+//       { transaction: t },
+//     );
+
+//     await Address.create(
+//       { city, street, number, employeeId: employee.id },
+//       { transaction: t },
+//     );
+
+//     // Se chegou até essa linha, quer dizer que nenhum erro ocorreu.
+//     // Com isso, podemos finalizar a transação usando a função `commit`.
+//     await t.commit();
+
+//     return res.status(201).json({ message: 'Cadastrado com sucesso' });
+//   } catch (e) {
+//     // Se entrou nesse bloco é porque alguma operação falhou.
+//     // Nesse caso, o sequelize irá reverter as operações anteriores com a função rollback, não sendo necessário fazer manualmente
+//     await t.rollback();
+//     console.log(e.message);
+//     res.status(500).json({ message: 'Algo deu errado' });
+//   }
+// });
 
 app.post('/employees', async (req, res) => {
   try {
     const { firstName, lastName, age, city, street, number } = req.body;
 
-    const employee = await Employee.create({ firstName, lastName, age });
+    const result = await sequelize.transaction(async (t) => {
+      const employee = await Employee.create({
+        firstName, lastName, age
+      }, { transaction: t });
 
-    await Address.create({ city, street, number, employeeId: employee.id });
+      await Address.create({
+        city, street, number, employeeId: employee.id
+      }, { transaction: t });
 
-    return res.status(201).json({ message: 'Cadastrado com sucesso' });
-  } catch (e) {
-    console.log(e.message);
-    res.status(500).json({ message: 'Algo deu errado' });
-  }
-});
+      return res.status(201).json({ message: 'Cadastrado com sucesso' });
+    });
 
-app.post('/employees', async (req, res) => {
-  // Primeiro iniciamos a transação
-  const t = await sequelize.transaction();
-
-  try {
-    const { firstName, lastName, age, city, street, number } = req.body;
-
-    // Depois executamos as operações
-    const employee = await Employee.create(
-      { firstName, lastName, age },
-      { transaction: t },
-    );
-
-    await Address.create(
-      { city, street, number, employeeId: employee.id },
-      { transaction: t },
-    );
-
-    // Se chegou até essa linha, quer dizer que nenhum erro ocorreu.
-    // Com isso, podemos finalizar a transação usando a função `commit`.
-    await t.commit();
-
-    return res.status(201).json({ message: 'Cadastrado com sucesso' });
+    // Se chegou até aqui é porque as operações foram concluídas com sucesso,
+    // não sendo necessário finalizar a transação manualmente.
+    // `result` terá o resultado da transação, no caso um empregado e o endereço cadastrado
   } catch (e) {
     // Se entrou nesse bloco é porque alguma operação falhou.
     // Nesse caso, o sequelize irá reverter as operações anteriores com a função rollback, não sendo necessário fazer manualmente
-    await t.rollback();
     console.log(e.message);
     res.status(500).json({ message: 'Algo deu errado' });
   }
