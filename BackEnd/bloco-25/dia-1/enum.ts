0baee72c3bfd88505bdd56ce6fbd44d2cce90f97
alvarogularte <alvaro.gularte@gmail.com>
2022-03-23 13:15:36 -0300

    conteudo da aula do dia 1

---
+++ BackEnd/bloco-25/dia-1/enum.ts
@@ -0,0 +1,34 @@
+enum WeekDays {
+  SUNDAY = 1,
+  MONDAY,
+  TUESDAY,
+  WEDNESDAY,
+  THURSDAY,
+  FRIDAY,
+  SATURDAY,
+}
+
+enum RainbowColors {
+  Red = "Vermelho",
+  Orange = "Laranja",
+  Yellow = "Amarelo",
+  Green = "Verde",
+  Blue = "Azul",
+  Indigo = "Anil",
+  Violet = "Violeta",
+}
+
+enum Actions {
+  SAVE,
+  PRINT,
+  OPEN,
+  VIEW,
+  CLOSE,
+}
+
+enum CardinalPoints {
+  North = "N",
+  East = "E",
+  South = "S",
+  West = "W",
+}
